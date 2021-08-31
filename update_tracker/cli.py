780a5595f082b219a8e6903f7327c81e3c7d0fc9
Independent-Dev <juju08217@daum.net>
2021-08-31 14:25:35 +0900

    8/30 작성한 코드 리팩토링

--- update_tracker/cli.py
+++ update_tracker/cli.py
@@ -6,11 +6,9 @@ from update_tracker.local import get_current_package_info, get_updated_package_i
 @click.option("--level", default="MICRO", help="지정된 단계까지 출력할 수 있도록 하는 옵션. MAJOR, MINOR, MICRO 가능. default는 MICRO")
 def track(verbose, level):
     """현재 PC에 설치된 파이썬 패키지의 업데이트 정보를 출력하는 명령어"""
-    current_package_info = get_current_package_info()
+    package_info = dict()  # TODO 에러 처리 부분은 좀 더 고민해보기
+    current_package_info = get_current_package_info()  # 이 경우에는 함수명도 add_current_package_info()와 같은 식으로 가야겠지
     updated_package_info = get_updated_package_info(current_package_info)
-    # package_info = dict()
-    # get_current_package_info(package_info)  # 이 경우에는 함수명도 add_current_package_info()와 같은 식으로 가야겠지
-    # get_updated_package_info(current_package_info)
-    result = compare_current_and_updated_package_info(updated_package_info)
+    result = compare_current_and_updated_package_info(updated_package_info, level)
     make_output(result, verbose, level)
     
--- update_tracker/local.py
+++ update_tracker/local.py
@@ -1,52 +1,51 @@
 import subprocess, requests, click
-import pdb
 from typing import List, Dict
 from update_tracker.utils import Level
 
-SEARCH_URL = "https://pypi.python.org/pypi/{}/json"
-
-def get_current_package_info() -> List[List[str]]:
+def get_current_package_info() -> Dict[str, Dict[str, str]]:
+    current_package_info = dict()
     pip_output_list = subprocess.check_output(['pip', 'list']).decode().strip().split("\n")
-    return [pip_output.split()[:2] for pip_output in pip_output_list][2:]
+    for pip_output in pip_output_list[2:]:
+        package_name, package_version = pip_output.split()[:2]
+        current_package_info[package_name] = {"current_version": package_version}
+    return current_package_info
 
-def get_updated_package_info(current_package_info: List[List[str]], verbose: bool) -> Dict[str, Dict[str, str]]:
-    updated_package_info = dict()
 
-    for package_name, _ in current_package_info:
+def get_updated_package_info(current_package_info: Dict[str, Dict[str, str]]) -> Dict[str, Dict[str, str]]:
+    SEARCH_URL = "https://pypi.python.org/pypi/{}/json"
+    
+    updated_package_info = dict(error=[])
+    for package_name, package_data in current_package_info.items():
         result = requests.get(SEARCH_URL.format(package_name))
         if result.status_code == 200:
-            updated_package_info[package_name] = dict()
-            result_json = result.json()
-            updated_package_info[package_name]["updated_version"] = result_json["info"]["version"]
-            if verbose:
-               updated_package_info[package_name]["upload_time"] = result_json["releases"][result_json["info"]["version"]]["upload_time"]
-        else:
-            if 'error' in updated_package_info.keys():
+            try:
+                result_json = result.json()
+                updated_package_info[package_name] = dict(**package_data)
+                updated_package_info[package_name]["updated_version"] = result_json["info"]["version"]
+                updated_package_info[package_name]["upload_time"] = result_json["releases"][result_json["info"]["version"]][0]["upload_time"]
+            except Exception as e:
                 updated_package_info['error'].append(package_name)
-            else:
-                updated_package_info['error'] = [package_name]
-    
+        else:
+            updated_package_info['error'].append(package_name)
+
     return updated_package_info
 
-def compare_current_and_updated_package_info(
-        current_package_info, updated_package_info, level, verbose
-    ) -> Dict[str, Dict[str, str]]:
+def compare_current_and_updated_package_info(updated_package_info: Dict[str, Dict[str, str]], level) -> Dict[str, Dict[str, str]]:
     result = [{} for _ in range(Level[level])]
-    for package_name, current_package_version in current_package_info:
-        if package_name not in updated_package_info["error"]:
-            current_package_version = current_package_version.split(".")
-            updated_package_version = updated_package_info[package_name]["updated_version"].split(".")
-            for i in range(min(Level[level], len(current_package_version))):
-                if current_package_version[i] != updated_package_version[i]:
-                    result[i][package_name] = updated_package_info[package_name]
-                    # if verbose:
-                    result[i][package_name]["current_version"] = current_package_version
-                    break
+    for package_name, package_data in updated_package_info.items():
+        if package_name != 'error':
+            if package_data["current_version"] != package_data["updated_version"]:  
+                current_package_version = package_data["current_version"].split(".")
+                updated_package_version = package_data["updated_version"].split(".")
+                for i in range(min(Level[level], len(current_package_version))):
+                    if current_package_version[i] != updated_package_version[i]:
+                        result[i][package_name] = package_data
+                        break
     
     result.append(updated_package_info["error"])
     return result
 
-def make_output(result, verbose, level) -> None:
+def make_output(result: Dict[str, Dict[str, str]], verbose, level) -> None:
     for level in Level:
         click.echo(f"{level.name}: {result[level.value - 1].keys()}")
     click.echo(f"error package: {result[-1]}")
